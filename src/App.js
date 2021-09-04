3489c92c179f31e8b29c1c02910f396e3457fc19
Vincent Kao <vincent0629@gmail.com>
2021-09-04 12:23:25 +0800

    Display level, score, and next block.

--- src/App.js
+++ src/App.js
@@ -20,6 +20,8 @@ const BLOCK_POS = [
   [[[0, 0], [1, 0], [0, 1], [1, 1]]]
 ];
 let paused = false;
+let level = 1;
+let score = 0;
 let state = NEW_BLOCK;
 let keys = [];
 let counter;
@@ -29,15 +31,21 @@ let blockAnchor;
 let blockAngle = 0;
 let blockModel = [];
 let rowSum = [];
+let nextBlockIndex;
+let nextBlockModel = [];
 
 function App() {
   const [blockSize, setBlockSize] = useState(20);
   // eslint-disable-next-line
   const [_blockModel, setBlockModel] = useState([]);
-  const [level, setLevel] = useState(0);
-  const [score, setScore] = useState(0);
   // eslint-disable-next-line
-  const [_paused, setPaused] = useState(false);
+  const [_level, setLevel] = useState(level);
+  // eslint-disable-next-line
+  const [_score, setScore] = useState(score);
+  // eslint-disable-next-line
+  const [_nextblockModel, setNextBlockModel] = useState([]);
+  // eslint-disable-next-line
+  const [_paused, setPaused] = useState(paused);
 
   const measureSize = () => {
     const w = Math.floor(window.innerWidth / BLOCK_COLUMN);
@@ -60,11 +68,15 @@ function App() {
     return false;
   };
 
-  const putBlock = (index) => {
-    const pos = BLOCK_POS[blockIndex][blockAngle];
+  const updateModel = (model, anchor, index, angle, value) => {
+    const pos = BLOCK_POS[index][angle];
     for (let i = 0; i < pos.length; ++i)
-      blockModel[blockAnchor[1] + pos[i][1]][blockAnchor[0] + pos[i][0]] = index;
+      model[anchor[1] + pos[i][1]][anchor[0] + pos[i][0]] = value;
   };
+
+  const putBlock = useCallback((value) => {
+    updateModel(blockModel, blockAnchor, blockIndex, blockAngle, value);
+  }, []);
 
   const moveBlock = useCallback((diff) => {
     putBlock(-1);
@@ -75,9 +87,9 @@ function App() {
     blockAnchor[0] += diff[0];
     blockAnchor[1] += diff[1];
     putBlock(blockIndex);
-    setBlockModel(Array.from(blockModel));
+    setBlockModel([blockModel]);
     return true;
-  }, []);
+  }, [putBlock]);
 
   const rotateBlock = useCallback((diff) => {
     const poses = BLOCK_POS[blockIndex];
@@ -91,9 +103,9 @@ function App() {
       return false;
     }
     putBlock(blockIndex);
-    setBlockModel(Array.from(blockModel));
+    setBlockModel([blockModel]);
     return true;
-  }, []);
+  }, [putBlock]);
 
   const resizeHandler = useCallback(() => {
     measureSize();
@@ -106,13 +118,19 @@ function App() {
   const timeHandler = useCallback(() => {
     switch (state) {
       case NEW_BLOCK:
-        blockIndex = Math.floor(Math.random() * 7);
+        blockIndex = nextBlockIndex !== undefined ? nextBlockIndex : Math.floor(Math.random() * BLOCK_POS.length);
         blockAngle = 0;
         blockAnchor = [Math.floor(BLOCK_COLUMN / 2), 0];
         counter = countdown;
         state = overlap([0, 0]) ? GAME_OVER : PLAYING;
         putBlock(blockIndex);
-        setBlockModel(Array.from(blockModel));
+        setBlockModel([blockModel]);
+
+        if (nextBlockIndex !== undefined)
+          updateModel(nextBlockModel, [1, 1], nextBlockIndex, 0, -1);
+        nextBlockIndex = Math.floor(Math.random() * BLOCK_POS.length);
+        updateModel(nextBlockModel, [1, 1], nextBlockIndex, 0, nextBlockIndex);
+        setNextBlockModel([nextBlockModel]);
         break;
       case PLAYING:
         if (keys.length > 0) {
@@ -149,7 +167,7 @@ function App() {
               blockModel[i][j] = -1;
           }
         }
-        setBlockModel(Array.from(blockModel));
+        setBlockModel([blockModel]);
         counter = 20;
         state = DELETED_ROWS;
         break;
@@ -164,6 +182,8 @@ function App() {
             else
               ++i;
           }
+          score += (BLOCK_ROW - rowSum.length) * 100;
+          setScore(score);
           for (let i = rowSum.length; i < BLOCK_ROW; ++i) {
             rowSum.unshift(0);
             const m = new Array(BLOCK_COLUMN);
@@ -171,32 +191,35 @@ function App() {
               m[j] = -1;
             blockModel.unshift(m);
           }
-          setBlockModel(Array.from(blockModel));
-          state = NEW_BLOCK;
+          setBlockModel([blockModel]);
+          state = score >= level * 500 ? NEXT_LEVEL : NEW_BLOCK;
         }
         break;
       case NEXT_LEVEL:
-        setLevel((lv) => {
-          if (lv >= 1)
-            countdown = Math.ceil(countdown * 0.8);
-          return lv + 1;
-        });
+        countdown = Math.ceil(countdown * 0.8);
+        setLevel(++level);
         state = NEW_BLOCK;
         break;
       case GAME_OVER:
         break;
       // no default
     }
-  }, [moveBlock, rotateBlock]);
+  }, [putBlock, moveBlock, rotateBlock]);
 
   useEffect(() => {
     blockModel = new Array(BLOCK_ROW);
     rowSum = new Array(BLOCK_ROW);
-    for (let i = 0; i < BLOCK_ROW; ++i) {
+    for (let i = 0; i < blockModel.length; ++i) {
       blockModel[i] = new Array(BLOCK_COLUMN);
-      for (let j = 0; j < BLOCK_COLUMN; ++j)
+      for (let j = 0; j < blockModel[i].length; ++j)
         blockModel[i][j] = -1;
       rowSum[i] = 0;
+    }
+    nextBlockModel = new Array(4);
+    for (let i = 0; i < nextBlockModel.length; ++i) {
+      nextBlockModel[i] = new Array(4);
+      for (let j = 0; j < nextBlockModel[i].length; ++j)
+        nextBlockModel[i][j] = -1;
     }
     measureSize();
     setBlockModel(blockModel);
@@ -206,9 +229,16 @@ function App() {
   }, [resizeHandler, timeHandler]);
 
   return (
-    <div className="App">
-      <BlockView size={blockSize} model={blockModel} />
-    </div>
+    <>
+      <div className="App">
+        <BlockView size={blockSize} model={blockModel} />
+      </div>
+      <div className="info">
+        <div className="level">Level {level}</div>
+        <div className="score">Score {score}</div>
+        <BlockView size={blockSize} model={nextBlockModel} />
+      </div>
+    </>
   );
 }
 
